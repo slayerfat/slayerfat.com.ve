fde7df39ea2e51d554c3e1a40d4953046c570d70
Alejandro Granadillo <slayerfat@gmail.com>
2016-05-01 15:08:11 -0400

    ajustado greeter con nuevos mensajes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,14 @@
 var Vue = require('vue');
 import Greeter from './components/Greeter.vue';
 
+Vue.filter('fontAwesomeClass', function (value) {
+    return 'fa fa-' + value;
+});
+
+Vue.filter('arrayRandom', function (value) {
+    return value[Math.floor(Math.random() * value.length)];
+});
+
 new Vue({
     el: '#app',
 
--- resources/assets/js/components/Greeter.vue
+++ resources/assets/js/components/Greeter.vue
@@ -4,11 +4,15 @@
             <div class="row">
                 <div class="column large-12">
                     <h1>{{ name }}</h1>
+                    <p>{{ currentDesc }}</p>
                 </div>
             </div>
             <div class="row align-spaced">
                 <div v-for="link in links" class="small-12 medium-2 columns">
-                    <a :href="link.url">{{ link.name }}</a>
+                    <a :href="link.url">
+                        <i :class="link.className | fontAwesomeClass"></i>
+                        {{ link.name }}
+                    </a>
                 </div>
             </div>
         </div>
@@ -21,15 +25,43 @@
             return {
                 name: 'Alejandro Granadillo Palau',
 
+                currentDesc: '',
+
+                descList: [
+                    'Programación, ingeniería de software, estructura de datos y mas.',
+                    'Creador del Código T, los Trululus, Maria Sofia y Cafe Pedro Lopez.',
+                    'Saaaaaaaaaaaludos a todos!',
+                    'Narrador, comentarista, aficionado de los números, amante de los Zerg.',
+                    'Creador de contenidos multimedia web 2.0',
+                    'Mago manipulador de entidades y relaciones nivel 10',
+                    'Modelo, Vista, Controlador, Entidad, Relación, Metodología.'
+                ],
+
                 links: [
-                    {name: 'Videos', url: '/videos'},
-                    {name: 'Blog', url: '/blog'},
-                    {name: 'Twitter', url: 'http://www.twitter.com/slayerfat'},
-                    {name: 'Facebook', url: 'http://www.facebook.com/slayerfat'},
-                    {name: 'G+', url: 'https://plus.google.com/112944288001461665013'},
-                    {name: 'Github', url: 'http://www.github.com/slayerfat'}
+                    {url: '/blog', className: 'pencil'},
+                    {url: '/videos', className: 'play'},
+                    {url: 'http://www.twitter.com/slayerfat', className: 'twitter'},
+                    {url: 'http://www.facebook.com/slayerfat', className: 'facebook'},
+                    {url: 'https://plus.google.com/112944288001461665013', className: 'google-plus'},
+                    {url: 'http://www.github.com/slayerfat', className: 'github'}
                 ]
             };
+        },
+
+        methods: {
+            changeDesc () {
+                return this.currentDesc = this.getRandomDesc();
+            },
+
+            getRandomDesc () {
+                return this.descList[Math.floor(Math.random() * this.descList.length)];
+            }
+        },
+
+        created() {
+            this.changeDesc();
+
+            setInterval(this.changeDesc, 10000);
         }
     };
 </script>
