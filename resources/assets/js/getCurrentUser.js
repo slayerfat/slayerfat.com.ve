15e61ad5bb4c2f663d1ea5df2fc37bab86b9a0ee
Alejandro Granadillo <slayerfat@gmail.com>
2016-05-07 10:16:33 -0400

    current user debe devolver un objeto si es nulo

--- resources/assets/js/getCurrentUser.js
+++ resources/assets/js/getCurrentUser.js
@@ -4,7 +4,11 @@ export function getCurrentUser(http) {
     // http://www.html5rocks.com/en/tutorials/es6/promises/
     return new Promise(function (resolve, reject) {
         http.get('users/current').then(function (response) {
-            resolve(response.data);
+            let data = response.data;
+            if (typeof data !== 'object') {
+                data = user;
+            }
+            resolve(data);
         }, function () {
             reject(user);
         });
